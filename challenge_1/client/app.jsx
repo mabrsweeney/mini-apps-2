a8c1aaffe4ad82abe2d957795c14fcaab7c563fe
mabrsweeney <mabrsweeney@gmail.com>
2018-04-04 10:54:32 -0700

    renders react component

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  componentDidMount() {
 
+  }
+
+  render() {
+  return (<h1>Hello, everyone!</h1>);
+  }
+}
+module.exports = App;
