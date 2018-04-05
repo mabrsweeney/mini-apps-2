f66984c40bed8872f437e34e32ed910ad60b571d
mabrsweeney <mabrsweeney@gmail.com>
2018-04-04 19:24:02 -0700

    displays various currencies

--- challenge_1/client/CurrencySelect.jsx
+++ challenge_1/client/CurrencySelect.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const CurrencySelect = (props) => {
-  return (<select>
-    {props.currencies.map((currency, index) => <option key={index} value={currency.currency}>{currency.country}</option>)}
+  return (<select value={props.currency} onChange={props.currencyChange}>
+    {props.currencies.map((currency, index) => {
+      return (
+        <option 
+          key={index} 
+          value={currency.currency}
+        >{currency.country}</option>);
+    })}
   </select>);
 };
 
