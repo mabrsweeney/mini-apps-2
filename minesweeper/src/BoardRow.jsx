e90f07b1701a9db55cb50e4d2ea2d63965c9359d
mabrsweeney <mabrsweeney@gmail.com>
2018-04-06 11:48:19 -0700

    passed down flag

--- minesweeper/src/BoardRow.jsx
+++ minesweeper/src/BoardRow.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Tile from './Tile';
 const BoardRow = props => {
     return (<div id="row-container">{props.row.map((tile, idx) => {
-      return (<Tile key={idx} x={props.x} y={idx} tile={tile} visible={props.vRow[idx]} tileClick={props.tileClick}/>);
+      return (<Tile key={idx} x={props.x} y={idx} tile={tile} visible={props.vRow[idx]} flag={props.flag}tileClick={props.tileClick}/>);
     })}</div>);
 }
 
